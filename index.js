e0b83fc204adf752409a78e85ac93789ac24109c
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2021-05-05 21:42:53 +0200

    fix: server fix

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
